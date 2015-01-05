89b2c02136e5d2b2d953acb744e6a1a45a5b34cd
timothy <tbradford16@my.whitworth.edu>
2015-01-05 15:46:45 -0800

    Finished PR1_1: Date Only
    Finished PR1_1: Date Only

---
+++ Home Work/Home Work 1/PR1 Date Only/src/pr1/date/only/PR1DateOnly.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr1.date.only;
+
+/**
+ *
+ * @author tbradford16
+ */
+public class PR1DateOnly {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+class DataOnly {
+
+            int i;
+            float f;
+            boolean b;
+        }
+    
+    DataOnly d = new DataOnly();
+    
+        d.i = 47;
+        d.f = 1.1f; // ‘f’ after number indicates float constant
+        d.b = false;
+        System.out.printf("PR1_1: Date Only\n");
+        
+        System.out.printf("int %d\nfloat %f\nboolean %b\n", d.i, d.f, d.b);
+        // This one
+    }
+    
+}
