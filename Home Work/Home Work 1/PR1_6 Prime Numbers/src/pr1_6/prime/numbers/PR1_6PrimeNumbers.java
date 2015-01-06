6bc7c8a696b3fda3813365b90524e1637ae855cc
timothy <tbradford16@my.whitworth.edu>
2015-01-06 10:17:37 -0800

    added compatibility for 3 and 2
    added compatibility for 3 and 2

--- Home Work/Home Work 1/PR1_6 Prime Numbers/src/pr1_6/prime/numbers/PR1_6PrimeNumbers.java
+++ Home Work/Home Work 1/PR1_6 Prime Numbers/src/pr1_6/prime/numbers/PR1_6PrimeNumbers.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pr1_6.prime.numbers;
 
 import java.util.Scanner;
@@ -19,30 +18,32 @@ public class PR1_6PrimeNumbers {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-         Scanner input = new Scanner(System.in);
-        
-         System.out.println(" Prime Number Checker\n Plesea enter a number to be checked");
-        
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(" Prime Number Checker\n Plesea enter a number to be checked");
+
         int prime = input.nextInt();
         boolean yes = true;
-        
+
         // I do not need to divide any number higher than half of the number given to find the answere
-        int count = prime/2;
-        
+        int count = prime / 2;
+
         //982,451,653 is a prime number used for testing
-        
-        for(int i = 2; i < count; i++){
-            
-        if(prime%i == 0 && i!=1 && i!= prime){
-        System.out.println(" This number is not a prime number");
-        yes = false;
-        break;
+        if (prime > 3) {
+            for (int i = 2; i < count; i++) {
+
+                if (prime % i == 0 && i != 1 && i != prime) {
+                    System.out.println(" This number is not a prime number");
+                    yes = false;
+                    break;
+                }
+            }
         }
+        if (yes == true) {
+            System.out.println(" You gave me a prime number");
         }
-        if(yes == true){
-        System.out.println(" You gave me a prime number");
-        }
+
     }
-    
+
 }
