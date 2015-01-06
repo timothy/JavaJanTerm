7d70b8939c9f972412c0f8df9e17472e92bf3910
timothy <tbradford16@my.whitworth.edu>
2015-01-05 16:33:37 -0800

    PR1_4AreaOfACircle
    Finds the Area of a circle

---
+++ Home Work/Home Work 1/PR1_2 Iterating the Arguments/src/pr1_2/iterating/the/arguments/PR1_2IteratingTheArguments.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr1_2.iterating.the.arguments;
+
+/**
+ *
+ * @author tbradford16
+ */
+public class PR1_2IteratingTheArguments {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // I do not know how to get here through the consol app yet?
+        System.out.printf("%s\n%s\n%s",args,args,args);
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ Home Work/Home Work 1/PR1_4 Area of a Circle/src/pr1_4/area/of/a/circle/PR1_4AreaOfACircle.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr1_4.area.of.a.circle;
+import java.util.Scanner;
+/**
+ *
+ * @author tbradford16
+ */
+public class PR1_4AreaOfACircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        
+        Scanner input = new Scanner(System.in);
+        
+         System.out.println(" Please give the radius of your circle\n");
+        
+        int radius = input.nextInt();
+        
+        double area = radius*radius * PI;
+        
+        System.out.printf("\nThe Area of this circle is %f \n", area);
+        
+        // TODO code application logic here
+    }
+    
+}
